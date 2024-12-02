3c851b32580f9062c4594208fedeb7857e5a6855
karelinaas <karelinaas95@gmail.com>
2024-12-02 18:19:56 +0300

    One more mistake in Unknown Company Bot's code...

--- examples/serializers/somecompanygpt.py
+++ examples/serializers/somecompanygpt.py
@@ -8,10 +8,10 @@ from referrals.models import ReferralStat
 
 class ReferralStatSerializer(serializers.ModelSerializer):
     is_campaign_active = serializers.SerializerMethodField()
-    # Правка 3 из 7: намудрил с определением поля
+    # Правка 3 из 8: намудрил с определением поля
     # total_earned = serializers.IntegerField(source='get_total_earned', read_only=True)
     total_earned = serializers.FloatField()
-    # Правка 7 из 7: вместо имени программы возвращалось ID
+    # Правка 7 из 8: вместо имени программы возвращалось ID
     campaign = serializers.CharField(source='campaign.title')
 
     class Meta:
@@ -19,7 +19,8 @@ class ReferralStatSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'campaign',
-            'affiliate',
+            # Правка 8 из 8: было лишнее поле affiliate
+            # 'affiliate',
             'referrals_number',
             'is_campaign_active',
             'total_earned',
@@ -29,11 +30,11 @@ class ReferralStatSerializer(serializers.ModelSerializer):
         current_date = datetime.date.today()
         return instance.campaign.active_since <= current_date <= instance.campaign.active_till
 
-    # Правка 6 из 7: неправильная логика подсчёта
+    # Правка 6 из 8: неправильная логика подсчёта
     def get_total_earned(self, instance):
-        # Правка 4 из 7: ошибка "Complex aggregates require an alias"
+        # Правка 4 из 8: ошибка "Complex aggregates require an alias"
         earnings = instance.affiliate.stats.aggregate(
             referrals_number__sum=Sum('referrals_number') * instance.campaign.reward
         )
-        # Правка 5 из 7: ошибка из-за earnings[0]
+        # Правка 5 из 8: ошибка из-за earnings[0]
         return earnings['referrals_number__sum'] * instance.campaign.reward if earnings else 0
--- examples/views/somecompanygpt.py
+++ examples/views/somecompanygpt.py
@@ -10,14 +10,14 @@ class ReferralStatsListAPIView(ListAPIView):
     serializer_class = ReferralStatSerializer
     queryset = ReferralStat.objects.all().order_by('-id')
 
-    # Правка 1 из 7: потерял permission_classes
+    # Правка 1 из 8: потерял permission_classes
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Правка 2 из 7: зачем-то искал пользователя еще раз по е-мейлу, хотя он уже есть в self.request
+        # Правка 2 из 8: зачем-то искал пользователя еще раз по е-мейлу, хотя он уже есть в self.request
         # affiliate = get_object_or_404(Affiliate, email=self.request.user.email)
         # return ReferralStat.objects.filter(affiliate=affiliate).order_by('-id')
         return ReferralStat.objects.filter(affiliate=self.request.user).annotate(
-            # Правка 6 из 7: неправильная логика подсчёта, добавляю сюда свой способ вычисления
+            # Правка 6 из 8: неправильная логика подсчёта, добавляю сюда свой способ вычисления
             total_earned=F('campaign__reward') * F('referrals_number'),
         ).order_by('-id')
